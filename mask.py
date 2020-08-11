198566f386731488122532d9ae2214d95e601637
gan-xiao-nan <68838605+gan-xiao-nan@users.noreply.github.com>
2020-08-11 16:25:10 +0800

    Delete mask.py

--- mask.py
+++
@@ -1,7 +0,0 @@
-import cv2
-
-img = cv2.imread(r'C:\Users\xiao-nan.gan\Desktop\autoLabel\images\test\BlueLongB_1_1_3.jpg')
-mask = cv2.imread(r'C:\Users\xiao-nan.gan\Desktop\autoLabel\images\test\BlueLongB_1_1_3.jpg',0)
-res = cv2.bitwise_and(img,img,mask = mask)
-cv2.imshow('resutt',res)
-cv2.waitKey(0)
